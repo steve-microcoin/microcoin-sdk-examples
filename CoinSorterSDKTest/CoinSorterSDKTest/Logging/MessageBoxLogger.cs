0a41a216418795f393f67322fb9803cc80752a11
Kareem Kashwaa <kareem@microcoin.com>
2017-05-23 10:34:16 +1000

    Add projects

---
+++ CoinSorterSDKTest/CoinSorterSDKTest/Logging/MessageBoxLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace Astrosys.Logging
+{
+    /// <summary>
+    /// An overly simplified messagebox logging mechanism used for testing
+    /// </summary>
+    public class MessageBoxLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ NoteValidatorSDKTest/NoteValidatorSDKTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Astrosys.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteValidatorSDKTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm(new LoggingForm()));
+        }
+    }
+}
